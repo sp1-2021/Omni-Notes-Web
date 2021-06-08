cbe4e5dbc23fb6a4c07b89c0e677f263034d617e
Kamil Wozniak <jestem.kamil.wozniak@gmail.com>
2021-06-08 20:05:46 +0200

    tests: basic e2e google auth testing

--- e2e/index.e2e.test.ts
+++ e2e/index.e2e.test.ts
@@ -1,11 +1,15 @@
-const { chromium } = require('playwright');
+const { firefox } = require('playwright');
+import dotenv from 'dotenv';
+
+
+const env = dotenv.config({ path: '.env.test' }).parsed;
 
 describe('basic test', () => {
   let browser;
   let page;
 
   beforeAll(async () => {
-    browser = await chromium.launch();
+    browser = await firefox.launch();
   });
   afterAll(async () => {
     await browser.close();
@@ -17,9 +21,23 @@ describe('basic test', () => {
     await page.close();
   });
 
-  it('should work', async () => {
+  it('Test sign in rendering', async () => {
     await page.goto('http://localhost:3000');
     expect(await page.title()).toBe('Sign In');
+  });
+
+  it('Test sign in', async () => {
+    await page.goto('http://localhost:3000');
+
+    await page.click('text=Sign in with Google');
+    await page.click('[aria-label="Adres e-mail lub telefon"]');
+
+    await page.fill('[aria-label="Adres e-mail lub telefon"]', env.GOOGLE_TEST_EMAIL);
+    await page.press('[aria-label="Adres e-mail lub telefon"]', 'Enter');
+
+    await page.click('[aria-label="Wpisz hasło"]');
+    await page.fill('[aria-label="Wpisz hasło"]', env.GOOGLE_TEST_PASSWORD);
+    await page.press('[aria-label="Wpisz hasło"]', 'Enter');
   });
 });
 
--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -8,17 +8,3 @@ afterAll(() => {
   nock.cleanAll();
   nock.restore();
 });
-
-// if (typeof window !== 'undefined') {
-//   window.matchMedia = jest.fn().mockImplementation((query) => {
-//     return {
-//       matches: false,
-//       media: query,
-//       onchange: null,
-//       addListener: jest.fn(),
-//       removeListener: jest.fn(),
-//     };
-//   });
-//   window.scroll = jest.fn();
-//   window.alert = jest.fn();
-// }
