9b59a9a0180706d85110a61dc7fadbb31af67910
Michał Bar <logout400@gmail.com>
2021-06-15 20:16:51 +0200

    feat(auth): allow to specify auth scope range via env file

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,22 +1,25 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
+const scope = [
+  'https://www.googleapis.com/auth/userinfo.profile',
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/drive.appdata',
+  'https://www.googleapis.com/auth/drive.file',
+  'https://www.googleapis.com/auth/drive.install',
+];
+
+if (process.env.GOOGLE_DRIVE_FULL_ACCESS_SCOPE_ENABLED === 'true') {
+  scope.push('https://www.googleapis.com/auth/drive');
+}
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      scope: [
-        'https://www.googleapis.com/auth/userinfo.profile',
-        'https://www.googleapis.com/auth/userinfo.email',
-        'https://www.googleapis.com/auth/drive.appdata',
-        'https://www.googleapis.com/auth/drive.file',
-        'https://www.googleapis.com/auth/drive.install',
-        'https://www.googleapis.com/auth/drive',
-      ].join(' '),
+      scope: scope.join(' '),
     }),
-    // ...add more providers here
   ],
   callbacks: {
     async jwt(token, user, account, profile, isNewUser) {
